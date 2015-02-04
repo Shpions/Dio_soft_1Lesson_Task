847ef1354d180ea9335060c3346141b24e22bc4c
Shpions <xxiii@ukr.net>
2015-02-04 22:14:18 +0200

    hw1

---
+++ src/Dio_soft_courses/Main.java
@@ -0,0 +1,10 @@
+package Dio_soft_courses;
+
+public class Main {
+    public static void main(String[] args) {
+        for(String s : args){
+            System.out.println(s);
+        }
+        System.out.println(args.length);
+    }
+}
